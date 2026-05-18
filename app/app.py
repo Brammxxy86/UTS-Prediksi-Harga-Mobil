8d6e8a6e55696b268a8674dacb8257b0c8d2d33a
Brammxxy86 <kikopino948@gmail.com>
2026-05-18 17:46:06 +0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,77 +1,51 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 import joblib
 import numpy as np
+import os
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-model=joblib.load(
-"../models/model_mobil.pkl"
-)
+# path aman untuk Railway dan lokal
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "..", "models", "model_mobil.pkl")
 
-@app.route(
-"/",
-methods=["GET","POST"]
-)
+model = joblib.load(MODEL_PATH)
 
+
+@app.route("/", methods=["GET", "POST"])
 def home():
 
-    hasil=None
+    hasil = None
 
-    if request.method=="POST":
+    if request.method == "POST":
 
-        umur=float(
-        request.form["umur"]
-        )
+        umur = float(request.form["umur"])
+        km = float(request.form["km"])
+        fuel = float(request.form["fuel"])
+        transmisi = float(request.form["transmisi"])
+        engine = float(request.form["engine"])
+        power = float(request.form["power"])
+        seats = float(request.form["seats"])
 
-        km=float(
-        request.form["km"]
-        )
-
-        fuel=float(
-        request.form["fuel"]
-        )
-
-        transmisi=float(
-        request.form["transmisi"]
-        )
-
-        engine=float(
-        request.form["engine"]
-        )
-
-        power=float(
-        request.form["power"]
-        )
-
-        seats=float(
-        request.form["seats"]
-        )
-
-
-        data=np.array([[
-
-        umur,
-        km,
-        fuel,
-        transmisi,
-        engine,
-        power,
-        seats
-
+        data = np.array([[
+            umur,
+            km,
+            fuel,
+            transmisi,
+            engine,
+            power,
+            seats
         ]])
 
-        pred=model.predict(data)
+        pred = model.predict(data)
 
-        hasil="₹ {:,}".format(
-        int(pred[0])
-        )
+        hasil = "₹ {:,}".format(int(pred[0]))
 
     return render_template(
-    "index.html",
-    hasil=hasil
+        "index.html",
+        hasil=hasil
     )
 
 
-if __name__=="__main__":
-
+if __name__ == "__main__":
     app.run(debug=True)
